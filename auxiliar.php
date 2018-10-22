fdf7923c2e5454667b85d5bfa03c8d6481f453f6
Manuel Alejandro Benítez <manuelalejandro.benitez@iesdonana.org>
2018-10-22 19:31:53 +0200

    Refactorizacion, tiene algunos fallos

--- auxiliar.php
+++ auxiliar.php
@@ -1,21 +1,29 @@
-<?php
-function mostrarError($mensaje){
-  echo "<h3>Error: $mensaje</h3>\n";
+<?php 
+function selected($op1, $op2)
+{
+   return $op1 == $op2 ? "selected" : "";
 }
-function operacion($op1, $op2, $op){
-  switch ($op) {
-      case '+':
-          $res = $op1 + $op2;
-          break;
-      case '-':
-          $res = $op1 - $op2;
-          break;
-      case '*':
-          $res = $op1 * $op2;
-          break;
-      case '/':
-          $res = $op1 / $op2;
-          break;
-    }?>
-    <h3>Resultado: <?= $res ?></h3>
-<?php }
+function formulario($op1, $op2, $op, $ops)
+{
+?>
+  <form action="" method="get">
+      <label for="op1">Primer operando: *</label>
+      <input id="op1" type="text" name="op1" value="<?= $op1 ?>">
+      <br />
+      <label for="op2">Segundo operando: *</label>
+      <input id="op2" type="text" name="op2" value="<?= $op2 ?>">
+      <br />
+      <label for="op">Operacion:</label>
+      <<select name="op">
+          <?php foreach ($ops as $o): ?>
+              <option value="<?= $o ?>" <?= selected($op, $o) ?> >
+                  <?= $o ?>
+              </option>
+          <?php endforeach ?>
+      </select><br/>
+      <br />
+      <input type="submit" value="Calcular" >
+      <br/>
+  </form>
+<?php
+}
--- index.php
+++ index.php
@@ -6,57 +6,66 @@
     </head>
     <body>
         <?php
-        require './auxiliar.php';
-        $op1 = isset($_GET['op1']) ? trim($_GET['op1']) : '0';
-        $op2 = isset($_GET['op2']) ? trim($_GET['op2']) : '0';
-        $op = isset($_GET['op']) ? trim($_GET['op']) : '""';
-        $res = "";
-        ?>
-        <form action="" method="get">
-            <label for="op1">Primer operando:</label>
-            <input id="op1" type="text" name="op1" value="<?= $op1 ?>">
-            <br />
-            <label for="op2">Segundo operando:</label>
-            <input id="op2" type="text" name="op2" value="<?= $op2 ?>">
-            <br />
-            <label for="op">Operacion:</label>
-            <select id="op" name="op">
-                <option value="">-- Seleccione --</option>
-            <?php
-             $datos = array("+","-","*","/");
+        require 'auxiliar.php';
 
-             for($i=0; $i<count($datos); $i++)
-             {
-                if($datos[$i]==$op)
-                {?>
-                   <option value='<?=$datos[$i]?>' selected><?=$datos[$i]?></option>
-                <?php }
-                else
-                {?>
-                    <option value='<?=$datos[$i]?>'><?=$datos[$i]?></option>
-                <?php }
-             }
-             ?>
-            </select>
-            <br />
-            <input type="submit" value="Calcular" >
-            <br/>
-        </form>
+        const OP = ['+','-','*','/'];
+        const PAR = ['op1','op2','op'];
 
-        <?php
-        if ($op1 == "" || $op2 == "") {
-            mostrarError('Falta alguno de los operandos.');
+        $error = [];
+
+        //  Comprobación de parámetros
+        $par = array_keys($_GET);
+        sort($par);
+
+        if(empty($_GET)){
+            $op1 = '0';
+            $op2 = '0';
+            $op = '+';
+        } elseif ($par == PAR) {
+            $op1 = trim($_GET['op1']);
+            $op2 = trim($_GET['op2']);
+            $op = trim($_GET['op']);
         } else {
-          if (!is_numeric($op1) || !is_numeric($op2)) {
-            mostrarError('Alguno de los operando no es un número.');
-        } else {
-          if (!$op == '+' || !$op == '-' || !$op == '*' || !$op == '/') {
-            mostrarError('La operación debe ser: +, -, *, /');
-          } else {
-            operacion($op1, $op2, $op);
-            }
+            $error[] = "Los parámetros recibidos no son los correctos.";
         }
-    }
-                ?>
+
+
+
+        if (empty($error)) {
+          if (!is_numeric($op1)) {
+            $error[] = "El primer operando no es un número.";
+          }
+          if (!is_numeric($op2)) {
+            $error[] = "El segundo operando no es un número.";
+          }
+          if (!in_array($op, OP)) {
+            $error[] = "El operador no es valido.";
+          }
+        }
+
+        formulario($op1, $op2, $op, OP);
+
+        if (empty($error)) :
+          $res = "";
+          switch ($op) {
+            case '+':
+              $res = $op1 + $op2;
+            break;
+
+            case '-':
+              $res = $op1 - $op2;
+            break;
+
+            case '*':
+              $res = $op1 * $op2;
+            break;
+
+            case '/':
+              $res = $op1 / $op2;
+            break;
+          }
+        ?>
+          <h3>Resultado: <?= $res ?></h3>
+        <?php endif; ?>
     </body>
 </html>
